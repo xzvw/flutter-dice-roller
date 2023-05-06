b85afd01a82cd9c9c0904266af71eb0de0e4224c
xzvw <128382145+xzvw@users.noreply.github.com>
2023-05-07 01:25:34 +0800

    Add gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello world!',
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purple,
+                Colors.deepPurple,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello world!',
+            ),
           ),
         ),
       ),
